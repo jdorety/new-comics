a4fb1aee07bb74aa6e21bd033a644270fea1b111
Jonathan Dorety <kyullog@gmail.com>
2020-02-03 21:46:01 -0800

    Refactor for styled components / add covers

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -1,12 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 export const InfoBox = props => {
   return (
-    <div className="box-container">
-      <h3>{props.title}</h3>
-      <span>{props.publisher}</span>
-      <span>{props.creators}</span>
-      <p>{props.description}</p>
-    </div>
+    <Box>
+      {/* <img alt="comic book cover" src={props.cover_url} /> */}
+      <ThumbNail src={props.cover_url} alt="comic book cover" />
+      <BookInfo>
+        <h3>{props.title}</h3>
+        <span>{props.publisher}</span>
+      </BookInfo>
+      {/* <span>{props.creators}</span>
+      <p>{props.description}</p> */}
+    </Box>
   );
 };
+
+const Box = styled.div`
+  max-width: 500px;
+  border: 2px solid limegreen;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const ThumbNail = styled.img`
+  max-width: 150px;
+  box-shadow: 5px 5px 4px grey;
+  margin: 10px 1%;
+  margin-right: 5%;
+`;
+
+const BookInfo = styled.div`
+  text-align: left;
+`;
