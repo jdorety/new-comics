cfae32bfe8befa5cbe4ed437d94479e0d882c7ca
Jonathan Dorety <kyullog@gmail.com>
2020-02-04 20:52:40 -0800

    Fresh MUI makeover

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import InfoBox from "./components/InfoBox";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
+
 import "./App.css";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    justifyContent: "center"
+  },
+  paper: {
+    width: 100,
+    height: 140
+    // height: 140,
+    // width: 100
+  },
+  thumb: {
+    width: 100,
+    height: 140
+  }
+}));
+
+const getThumbUrl = url => {
+  let lastIndex = url.length - 1;
+  let thumb = url.slice(0, lastIndex);
+  return thumb + "3";
+};
 
 function App() {
   const [books, setBooks] = useState([]);
   const [covers, setCovers] = useState({});
+  const classes = useStyles();
 
   useEffect(() => {
     axios
@@ -25,7 +51,7 @@ function App() {
 
   return (
     <div className="App">
-      {books.map(book => {
+      {/* {books.map(book => {
         if (covers[book.diamond_id]) {
           let url = covers[book.diamond_id];
           let lastIndex = url.length - 1;
@@ -35,7 +61,26 @@ function App() {
         } else {
           return <InfoBox {...book} key={book.diamond_id} />;
         }
-      })}
+      })} */}
+      <Grid container className={classes.root} spacing={2}>
+        {books.map(book => {
+          let url = covers[book.diamond_id];
+          if (url) {
+            url = getThumbUrl(url);
+            return (
+              <Grid item xsUp={12} key={url}>
+                <Paper url={url} className={classes.paper}>
+                  <img
+                    className={classes.thumb}
+                    src={url}
+                    alt="comic book cover"
+                  />
+                </Paper>
+              </Grid>
+            );
+          }
+        })}
+      </Grid>
     </div>
   );
 }
