dd9d933cb6a7c049218bfcfebfae0a1b31f50253
Jonathan Dorety <kyullog@gmail.com>
2020-02-04 20:31:10 -0800

    Need to center that app!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 require("dotenv").config();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div className="app-wrapper">
+    <App />
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
